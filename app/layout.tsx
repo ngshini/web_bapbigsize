6f22e5e89e30115e32e0e0f7b387bd046cefa292
ngshini <ngshin6905@gmail.com>
2026-06-20 12:57:29 +0700

    icon logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Tổng kho đồ bộ miền Nam - Bắp Bigsize",
-  description: "Đồ bộ nữ big size, thời trang nữ big size form 45-85kg."
+  description: "Đồ bộ nữ big size, thời trang nữ big size form 45-85kg.",
+  icons: {
+    icon: "/picture/logo.jpg",
+    apple: "/picture/logo.jpg",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
